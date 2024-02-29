389e9fee21e9e984a01fb4082095a1b9fbde79a3
chizTheDeveloper <61754907+chizTheDeveloper@users.noreply.github.com>
2024-02-29 12:40:24 -0500

    Created Repo

---
+++ Oxford_Flowers-project/Oxford_Flowers/configs/mobile/mobilenet-v2_8xb32_in1k.py
@@ -0,0 +1,6 @@
+_base_ = [
+    '/content/drive/MyDrive/Oxford_Flowers/configs/mobile/mobilenet_v2_1x.py',
+    '/content/drive/MyDrive/Oxford_Flowers/configs/mobile/imagenet_bs32_pil_resize.py',
+    '/content/drive/MyDrive/Oxford_Flowers/configs/resnet/imagenet_bs256.py',
+    '/content/drive/MyDrive/Oxford_Flowers/configs/default_runtime.py'
+]
---
+++ Oxford_Flowers-project/Oxford_Flowers/configs/resnet/resnet34_8xb32_in1k.py
@@ -0,0 +1,6 @@
+_base_ = [
+    '/content/drive/MyDrive/Oxford_Flowers/configs/resnet/resnet34.py',
+     '/content/drive/MyDrive/Oxford_Flowers/configs/resnet/imagenet_bs32.py',
+    '/content/drive/MyDrive/Oxford_Flowers/configs/resnet/imagenet_bs256.py',
+     '/content/drive/MyDrive/Oxford_Flowers/configs/default_runtime.py'
+]
